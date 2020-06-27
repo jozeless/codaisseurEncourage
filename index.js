770c10703662b474720052afd18bcb58f6fdd12e
jozeless <jlrodriguezsanabria@gmail.com>
2020-06-27 16:52:53 +0200

    cambio de mensaje

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const message = `**
 ** Code is not what you know, but what you can figure out!!!, 
-** Practis Beat Talent!!!`
+** Practis Beat Talent!!!
+*** First week done learning a lot, cant belive i am the same person i was last saturday!!!
+*** maybe at the ende of the course i can build anithing`
 console.log(message)
